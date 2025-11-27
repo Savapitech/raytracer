528d040a452faf37d0bc6ac604a2d7628edc6634
Savinien Petitjean <savinien.petitjean@gmail.com>
2025-11-27 11:14:47 +0100

    Refactor Makefile

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
+
+    forAllSystems = function:
+      lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        hardeningDisable = ["fortify"];
+
+        packages = with pkgs; [
+          sfml
+          compiledb
+          clang
+        ];
+
+        env.NIX_CFLAGS_COMPILE =
+          let
+            pyVersion = lib.versions.majorMinor pkgs.python3.version;
+          in
+          "-isystem ${pkgs.python3}/include/python${pyVersion}";
+      };
+    });
+
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+  };
+}
