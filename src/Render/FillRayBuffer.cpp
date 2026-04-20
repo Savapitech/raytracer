396be8cdd19b1a76413f47dc5371ab5c58c50e65
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-04-21 01:57:40 +0200

    Add Path Tracing
    Templating the methode shade and appliedPBR for avoiding Branch
    Misprediction.
    Fix the return Value of shade, who now return Vec3.
    Add sampling into fillRayBuffer for a better accuracy of path Tracing.

--- src/Render/FillRayBuffer.cpp
+++ src/Render/FillRayBuffer.cpp
@@ -13,18 +13,46 @@ void Render::writePixel(int x, int y, sf::Color color) noexcept
 
 void Render::fillRayBuffer(float offsetX, float offsetY, int x, int y) noexcept
 {
-    float dirX = (2.0f * (offsetX + 0.5f) * _invWidth - 1.0f) * _aspect * _scale;
-    float dirY = (1.0f - 2.0f * (offsetY + 0.5f) * _invHeight) * _scale;
+    int samplesPerPixel = 64;
+    Vec3 accumulatedLight(0.0f, 0.0f, 0.0f);
 
-    Vec3 pixelDir = normalize(scene.getCamera().forward + scene.getCamera().right * dirX + scene.getCamera().up * dirY);
-    Ray ray(scene.getCamera().pos, pixelDir);
-    Hit minHit;
+    for (int s = 0; s < samplesPerPixel; ++s) {
+        float jitterX = fastRandomFloat(-0.5f, 0.5f);
+        float jitterY = fastRandomFloat(-0.5f, 0.5f);
 
-    this->bvh.intersect(ray, minHit);
-    if (minHit.ObjectIdx != -1) {
-        sf::Color color = this->shade(ray, minHit, 1000);
-        writePixel(x, y, color);
+        float dirX = (2.0f * (offsetX + jitterX + 0.5f) * _invWidth - 1.0f) * _aspect * _scale;
+        float dirY = (1.0f - 2.0f * (offsetY + jitterY + 0.5f) * _invHeight) * _scale;
+
+        Vec3 pixelDir = normalize(scene.getCamera().forward + scene.getCamera().right * dirX + scene.getCamera().up * dirY);
+        Ray ray(scene.getCamera().pos, pixelDir);
+        Hit minHit;
+
+        this->bvh.intersect(ray, minHit);
+        
+        if (minHit.ObjectIdx != -1) {
+            if (this->_isPathTracing) {
+                accumulatedLight += this->shade<true>(ray, minHit, 5);
+            } else {
+                accumulatedLight += this->shade<false>(ray, minHit, 5);
+            }
+        } else {
+            //accumulatedLight += Vec3(0.5f, 0.7f, 1.0f); Coord UV for the background 
+        }
     }
-    else
-        writePixel(x, y, {0, 0, 0, 255});
+    Vec3 averageLight = accumulatedLight / (float)samplesPerPixel;
+
+    averageLight.x = averageLight.x / (averageLight.x + 1.0f);
+    averageLight.y = averageLight.y / (averageLight.y + 1.0f);
+    averageLight.z = averageLight.z / (averageLight.z + 1.0f);
+
+    averageLight = averageLight * 255.0f;
+
+    sf::Color finalColor(
+        std::clamp((int)averageLight.x, 0, 255),
+        std::clamp((int)averageLight.y, 0, 255),
+        std::clamp((int)averageLight.z, 0, 255),
+        255
+    );
+
+    writePixel(x, y, finalColor);
 }
