c396fa64f4f85e03eca0b58fc3ddf26fe86cbc50
titouanhct <titouan.hochet@epitech.eu>
2026-04-20 15:48:30 +0200

    add half of  ambiant and directional to factory

--- src/Light/LightFactory.cpp
+++ src/Light/LightFactory.cpp
@@ -1,7 +1,20 @@
+#include "ILight.hpp"
 #include <libconfig.h++>
 #include <memory>
 #include <map>
 #include <functional>
 
 
+using LightCtor = std::function<std::unique_ptr<ILight>(const libconfig::Setting &s)>;
 
+std::map<std::string, LightCtor> lightRegistry;
+/*
+LightFactory::LightFactory() {
+  lightRegistry["Directional"] = [](const libconfig::Setting& s) {
+    return std::make_unique<Directional>(s);
+  };
+  lightRegistry["Ambiant"] = [](const libconfig::Setting& s) {
+    return std::make_unique<Ambiant>(s);
+  };
+}
+*/
